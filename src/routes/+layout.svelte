5a7ca2918766dbc863feb154f7b17ff42990e498
이상훈 <lsh93620@gmail.com>
2023-08-03 13:54:41 +0900

    eclozer page

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,89 @@
+
+<slot />
+
+<footer id="main-footer">
+    <section class="footer">
+        <div id="footer-logo">
+            <img src="/src/images/eclozer-logo-white.png" alt="회사 로고">
+        </div>
+        <div class="company">
+            <div class="company-title">이클로저</div>
+            <div>사업자 등록번호 : 444-48-00221</div>
+            <div>대표이사 : 김세현</div>
+            <div>주소 : 서울시 강남구 역삼로 29길 19</div>
+            <div>연락처 :  070-8648-1179</div>
+        </div>
+        <div class="copy">
+            <p>© 2023 Copyright by ECLOZER</p>
+        </div>  
+    </section>
+</footer>
+
+
+<style>
+    #main-footer{
+    height: auto;
+    display: flex;
+    justify-content: right;
+    background-color: #000;
+    padding:  0 5%;
+    color: #fff;
+}
+
+.footer{
+    width: 60%;
+    margin: 0 auto;
+    display: flex;
+    flex-flow: column;
+    justify-content: space-between;
+    align-items: center;
+    font-size: 0.5rem;
+    color: lightgrey;
+}
+
+#footer-logo{
+    margin: 1rem 4rem;
+    height: 3.5rem;
+    margin-bottom: 2rem;
+}
+
+#footer-logo img{
+    height: 4.5rem;
+}
+
+/* .company{
+    margin-left: 4rem;
+} */
+.company div{
+    font-size: 17px;
+    text-align: center;
+    /* margin-top: 1rem; */
+    padding: 0.2rem;
+}
+
+
+.copy{
+    margin: 2rem 0;
+    text-align: center;
+}
+
+@media (max-width:628px){
+    #footer-logo img{
+        margin: 1rem;
+        height: 3.5rem;
+        margin-left: 0;
+    }
+    .company{
+        margin-left: 0;
+    }
+
+    .copy{
+        margin: 0;
+        padding: 0;
+    }
+}
+
+
+</style>
+
+
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,580 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<main>
+    <!-- <div class="cover-bg">
+        <div class="eclozer-btn">
+            <a href="http://pf.kakao.com/_vgxlUT" target="_blank">ECLOZER</a>
+        </div>   
+    </div> -->
+
+    <section id="main-header">
+        <div class="header-content">
+            <div class="header-con-logo">
+                <img src="/src/images/eclozer-logo-white.png" alt="">
+            </div>
+            <h2 class="header-h2">
+               항상 고객의 소리에 귀를 기울입니다.<br>
+               지금 바로 문의주세요.
+            </h2>
+            <div class="header-link-btn">
+                <a href="http://pf.kakao.com/_vgxlUT" target="_blank">카카오채널 바로가기</a>
+            </div>
+            <div class="header-platform-text">
+                <div>
+                    <div class="platform-name">IT 개발</div>
+                </div>
+                <div>
+                    <div class="platform-name">블록체인</div>
+                </div>
+                <div>
+                    <div class="platform-name">FX 솔루션</div>
+                </div>
+            </div>
+        </div>
+    </section> 
+
+    <section id="platform-business">
+        <div class="platform-business-content">
+            <h1>flatform-business</h1>
+            <div>
+                <div class="platform-business-img">
+                    <img src="/src/images/floatform-img.png" alt="">
+                </div>
+                <div class="platform-business-fx">
+                    <div class="platform-business-fx-intro">
+                            <p>Supported instrument types:</p>
+                            <p>Equities, ETFs, Futures, Options, Bonds, Forex, CFDs.</p>
+                            <hr style="width: 10px;">  
+                            <p><strong>The eclozer's system</strong></p>
+                            <p>allows trading and trading of both <strong>exchange-traded</strong> and <strong>over-the-counter derivatives</strong>, </p>
+                            <p>and has all the core features that include the lifecycle of each of the aforementioned types of products.</p>
+                        
+                    </div>
+                    <div>
+                        <ul class="platform-business-fx-partner">
+                             <li>partner company</li>
+                             <li>
+                                 <img src="/src/images/LMAX-bo-group.png" alt="">
+                             </li>
+                             <li>
+                                 <img src="/src/images/identifier_RETINA.png" alt="">
+                             </li>
+                             <li>
+                                 <img src="/src/images/bg-evo-logo_0.png" alt="">
+                             </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    
+
+    <section id="platform-item">
+        <div class="platform-item-content">
+            <div class="platform-item-txt">
+                <h2 class="platform-item-title">웹기반 P2P 서비스</h2>
+                <div class="platform-item">
+                    <ul class="platform-p2p-list">
+                        <li>디지털 상품을 개인간 P2P 거래할 수 있도록 에스크로 해주는 플랫폼</li>
+                        <li>이메일 인증, PASS 핸드폰 본인인증, 은행 실계좌인증, 신분증 인증</li>
+                    </ul>
+                    <div class="platform-link-box">
+                        <a href="https://fynx.cc/">fynx</a>
+                    </div>
+                </div>
+            </div>
+            <div class="platform-item-img">
+                <img src="src\images\platform-mockup-item01.png" alt="">
+            </div>
+        </div>
+    </section>
+    <section id="platform-item">
+        <div class="platform-item-content">
+            <div class="platform-item-img">
+                <img src="src\images\platform-mockup-item02.png" alt="">
+            </div>
+            <div class="platform-item-txt">
+                <h2 class="platform-item-title">Chat GPT 응용 앱개발</h2>
+                <div class="platform-item">
+                    <p>iOS, Android 버전 등록, 채팅, 요약, 글쓰기, Ai 이미지생성, D-day 기능의 개인화된 서비스 제공</p>
+
+                    <p>ai의 이미지를 등록하고 친구와 대화하면 고민이 순식간에 해결됩니다. 주제에 대한 글쓰기, 장문의 글 요약, SNS나 다이어리를 꾸밀 수 있는 이미지 제작을 지원합니다.</p>
+                    <div class="platform-link-box">
+                       <a href="https://play.google.com/store/apps/details?id=com.ai.wonders&pli=1">AI Wonders (Android)</a><br>
+                       <a href="https://apps.apple.com/us/app/ai-wonders/id6450855725">AI Wonders(Ios)</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section id="platform-item">
+        <div class="platform-item-content">
+            <div class="platform-item-img">
+                <img id="vpn-image" src="src\images\platform-mockup-item03.png" alt="">
+            </div>
+            <div class="platform-item-txt">
+                <h2 class="platform-item-title">VPN앱 서비스</h2>
+                <div class="platform-item">
+                    <p>무제한 VPN 프록시로 Double Rocket VPN 서비스를 무료로 제공합니다.
+                    </p>
+                    <div class="platform-link-box">
+                       <a href="https://play.google.com/store/apps/details?id=com.developer.doublerochet&hl=ko-KR">Double Rocket VPN</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section id="history">
+        <div class="history-content">
+            <h1>HISTORY</h1>
+            <div class="history-content-list">
+                <div class="history-content-title">연혁</div>
+                <div class="history-content-contents">
+                   <ul>
+                        <li><span class="strong">2021</span>코인지갑앱 및 코인 관련 프로젝트 다수 진행</li>
+                        <li><span class="strong">2020</span>가상화폐비즈니스 관련 컨설팅 및 코인 자동거래 프로그램 개발 및 판매</li>
+                        <li><span class="strong">2019</span>센터코인 메인넷개발 및 코인거래소상장</li>
+                        <li><span class="strong">2018</span>그래픽카드 도매 및 채굴기 납품 – 연 매출 5억원</li>
+                        <li><span class="strong">2017</span>양주시 이더리움 및 대시코인 채굴공장 운영</li>
+                   </ul>
+                </div>
+            </div>
+        </div>
+    </section>
+</main>
+<style>
+    @import url('https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@300;400;500;700&family=Roboto:wght@300;400;500;700&display=swap');
+
+    main{font-family: 'Roboto', sans-serif;}
+    h1{font-size: 4rem;}
+    li{list-style: none;}
+    #main-header{
+        width: 100%;
+        height: auto;
+        background-color: #021026;
+    }
+    .header-content{
+        padding: 150px 0;
+        width: 40%;
+        height: 100%;
+        /* background-color: lightgrey; */
+        margin: 0 auto;
+        display: flex;
+        flex-flow: column;
+        box-sizing: border-box;
+    }
+
+    .header-con-logo{
+        width: 300px;
+        height: 75px;
+        padding-bottom: 50px;
+    }
+
+    .header-con-logo img{
+        width: 100%;
+        margin-left: -10px;
+    }
+
+    .header-h2{
+        color: #ffffff;
+        font-size: 40px;
+        padding-bottom: 100px;
+    }
+    .header-link-btn{
+        width: 14rem;
+        height: 4rem;
+        border:3px solid #ffffff;
+        /* box-shadow: rgba(202, 202, 202, 0.2) 0px 60px 40px -7px; */
+        border-radius: 60px;
+        text-align: center;
+        margin-bottom: 200px;
+    }
+
+    .header-link-btn a{
+        text-align: center;
+        line-height: 4rem;
+        font-weight: bold;
+        font-size: 1.2rem;
+        color: #fff;
+    }
+
+    .header-platform-text{
+        width: 100%;
+        /* background-color: #ffffff; */
+        display: flex;
+        flex-flow: row wrap;
+        justify-content: space-between;
+    }
+
+    .header-platform-text>div{
+        width: 234px;
+        height: 304px;
+        border: 2px solid #ffff;
+      
+    }
+
+    .platform-name{
+        color: #fff;
+        font-size: 20px;
+        text-align: center;
+        padding: 20px;
+    }
+
+    .platform-name::after{
+        content: "";
+        display: block;
+        border: 1px solid #fff;
+        width: 20px;
+        margin: 20px auto;
+    }
+
+
+
+    #platform-business{
+        width: 100%;
+        height: auto;
+        background-color: white;
+    }
+
+    .platform-business-content{
+        padding: 50px 0 0 0;
+        width: 65%;
+        height: 100%;
+        /* background-color: lightgrey; */
+        margin: 0 auto;
+        box-sizing: border-box;
+        text-align: center;
+        text-transform: uppercase;
+    }
+
+    .platform-business-content h1{
+        margin: 100px 0;
+    }
+
+    .platform-business-img{
+        width: 100%;
+        height: 100%;
+    }
+
+    .platform-business-img img{
+        width: 100%;
+    }
+
+
+    .platform-business-fx{
+        height: 500px;
+        /* background-color: aliceblue; */
+        text-align: center;
+        margin: 100px auto;
+        display: flex;
+        flex-flow: column;
+        align-items: center;
+    }
+    .platform-business-fx-intro{
+        font-size: 1.5vw;
+    }
+
+    .platform-business-fx-intro p{
+        font-size: 1vw;
+    }
+
+    .platform-business-fx-partner{
+        display: flex;
+        flex-flow: row wrap;
+        margin-top: 45px;
+    }
+
+
+    .platform-business-fx-partner li{
+        list-style: none;
+        width: 150px;
+        padding: 0 20px;
+    }
+    
+    .platform-business-fx-partner li img{
+        width: 100%;
+    }
+
+    .platform-business-fx-partner li:nth-child(1){
+        padding: 10px;
+    }
+    .platform-business-fx-partner li:nth-child(3) img{
+        padding: 15px 0;
+    }
+    .platform-business-fx-partner li:nth-child(4) img{
+        padding: 6px 0;
+    }
+
+
+    #platform-item{
+        width: 100%;
+        margin: 300px 0;
+    }
+
+    #platform-item::after{
+        content: "";
+        display: block;
+        width: 20px;
+        border: 1px solid black;
+        margin: 20rem auto;
+
+    }
+    .platform-p2p-list{
+        padding: 0;
+        height: 84px;
+    }
+
+    .platform-p2p-list li{
+        font-size: 0.9vw;
+    }
+
+    .platform-item-content{
+        width: 60%;
+        height: 100%;
+        margin: 0 auto;
+        display: flex;
+        flex-flow: row wrap ;
+        justify-content: center;
+    }
+
+    .platform-item-txt{
+        width: 50%;
+        height: auto;
+        text-align: center;
+        /* background-color: lightgray; */
+    }
+
+    .platform-item-img{
+        width: 34.5rem;
+        /* background-color: pink; */
+        display: block;
+        margin: 0 auto;
+    }
+    .platform-item-img img{
+        width: 100%;
+    }
+
+    #vpn-image{
+        width: 80%;
+    }
+    
+
+    .platform-link-box{   
+        display: flex;
+        justify-content: center;
+    }
+
+    .platform-link-box a{
+        text-decoration: none;
+        border: 1px solid black;
+        padding: 10px 15px;
+        margin-right: 10px;
+        border-radius: 10px;
+    }
+
+
+    #history{
+        margin-top: 100px;
+        width: 100%;
+        height: auto;
+        /* background-color: white; */
+    }
+    .history-content{
+        width: 40%;
+        height: 100%;
+        /* background-color: lightgrey; */
+        margin: 0 auto;
+        display: flex;
+        flex-flow: column;
+       
+    }
+
+    .history-content h1{
+        margin: 100px 0;
+        text-align: center;
+    }
+
+
+    .history-content-list{
+        width: 100%;
+        height: 100%;
+        /* background-color: lightgrey; */
+        margin: 100px auto;
+        display: flex;
+        flex-flow: row wrap;
+        box-sizing: border-box;
+    }
+
+
+
+    .history-content-title{
+        flex-grow: 1.5;
+        margin: 16px 0;
+        font-size: 1.5rem;
+    }
+
+    .history-content-contents{
+        flex-grow: 2;
+    }
+    .history-content-contents>ul li{
+        list-style: none;
+        margin: 8px 0;
+        font-size: 1.2rem;
+      
+    }
+
+    .history-content-contents>ul li .strong{
+        font-weight: bold;
+        padding:  0 8px 0 0;
+        display: block;
+        font-size: 2rem;
+        
+    }
+
+
+/* --------------------------------반응형 --------------------------------------------------- */
+   
+    @media (max-width:1650px){
+        .header-h2{
+            font-size: 2vw;
+        }
+        a{
+            font-size: 1.2vw;
+        }
+
+        .platform-business-fx-intro{
+            font-size: 1.2vw;
+        }
+        .platform-item-title{
+             font-size: 1vw;
+       }
+    }
+    @media (max-width:1024px){
+        h1{font-size: 6vw;}
+        
+        .header-h2{
+            font-size: 1.7vw;
+        }
+        a{
+            font-size: 1.5vw;
+        }
+
+        .platform-business-fx-intro p{
+            font-size: 0.5vw;
+        }
+
+        .platform-item-title{
+            font-size: 1.5vw;
+        }
+
+        .platform-item-txt{
+            width: 100%;
+            text-align: center;
+        }
+    }    
+
+    @media (max-width:680px){
+        .header-content{
+            width: 90%;
+        }
+
+        .header-con-logo{
+            margin: 0 auto;
+        }
+
+        .header-con-logo img{
+            display: block;
+            margin: 0 auto;
+        }
+
+        .header-h2{
+            font-size: 1.0rem;
+            text-align: center;
+        }
+
+        .header-link-btn{
+            margin: 0 auto 100px auto;
+        }
+
+        .header-platform-text{
+            flex-flow: column;
+        }
+
+       .header-platform-text>div{
+            margin: 50px auto 0  auto;
+       }
+          
+        .platform-business-fx-partner{
+            flex-flow: column;
+        }
+
+        .platform-item-content{
+            width: 60%;
+            flex-flow: column;
+            justify-content: center;
+        }
+
+        .platform-item-txt{
+            width: 90%;
+            margin: 0 auto;
+        }
+
+        .platform-item-img{
+            width: 90%;
+        }
+
+        .platform-item-img img{
+            width: 100%;
+        }
+
+        .platform-item-title{
+            font-size: 1vw;
+        }
+        .platform-item-txt h2{
+            text-align: center;
+        }
+
+
+        .history-content{
+            width: 90%;
+        }
+
+        .history-content-title{
+            text-align: center;
+        }
+    }
+    
+    @media (max-width:375px){
+        h1{
+            font-size: 2vw;
+        }
+        .header-h2{
+            font-size: 2vw;
+        }
+
+        .platform-business-content{
+            width: 90%;
+        }
+
+        .platform-business-fx{
+            height: 100px;
+            margin: 30px auto;
+        }
+
+        #platform-item{
+            margin-top: 400px;
+        }
+
+
+        .platform-item-content{
+        
+            width: 90%;
+            
+        }
+    }
+
+   
+    
+    
+    
+    </style>
